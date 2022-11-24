ff13bfbcab1ba4a04c28008243d6ae1feca2c660
swayamshreemohanty <mohantyswayamshree@gmail.com>
2022-11-24 08:23:48 +0530

    decimal to binary added

--- BinDecConverter.java
+++ BinDecConverter.java
@@ -1,18 +1,31 @@
 public class BinDecConverter {
 
-    public static double binaryToDecimal(int binary) {
-        double dec = 0;
+    public static int binaryToDecimal(int binary) {
+        int dec = 0;
         int pow = 0;
         while (binary > 0) {
-            int lastdigit = binary % 10;
+            int remainder = binary % 10; // (extract the last digit)
             binary /= 10;
-            dec += (lastdigit * Math.pow(2, pow));
+            dec += (remainder * (int) Math.pow(2, pow));
             pow++;
         }
         return dec;
     }
 
+    public static int decimalToBinary(int decimal) {
+        int binary = 0;
+        int pow = 0;
+        while (decimal > 0) {
+            int remainder = decimal % 2;
+            binary += remainder * (int) Math.pow(10, pow);
+            decimal /= 2;
+            pow++;
+        }
+        return binary;
+    }
+
     public static void main(String[] args) {
-        System.out.println(binaryToDecimal(1000));
+        System.out.println(decimalToBinary(12));
+
     }
 }
