316551391bcbab9df79fe4e7ff20075d2200d2d9
Swayamshree <mohantyswayamshree@gmail.com>
2023-01-01 23:21:47 +0530

    code updated

--- array_assignment/SearchInRoatedArray.java
+++ array_assignment/SearchInRoatedArray.java
@@ -3,59 +3,66 @@
 //Binary search method
 
 public class SearchInRoatedArray {
-    public static int searchElementIndex(int[] nums, int target) {
-        int minimumElementIndex = searchMinimumNumberIndex(nums);
-        if (nums[minimumElementIndex] <= target && target < nums.length) {
-            // Left side sorted array
-            return searchElementIndexInRange(nums, minimumElementIndex, nums.length - 1, target);
-        } else {
-            // Right side sorted array
-            return searchElementIndexInRange(nums, 0, minimumElementIndex, target);
-        }
 
+    public static int searchIndex(int[] nums, int target) {
+        int minimumElementIndex = findMinElementIndex(nums);
+
+        if (target >= nums[minimumElementIndex] && target <= nums[nums.length - 1]) {
+            // Find in Right sorted array
+            return findElementInRange(nums, minimumElementIndex, nums.length - 1, target);
+        } else {
+            // Find in Left sorted array
+            return findElementInRange(nums, 0, minimumElementIndex - 1, target);
+
+        }
     }
 
-    public static int searchElementIndexInRange(int[] nums, int startIndex, int endIndex, int target) {
+    // Helper function
+    public static int findElementInRange(int[] nums, int startIndex, int endIndex, int target) {
+        int elementIndex = -1;
         int leftIndex = startIndex;
-        int rightIndex = endIndex;
+        int rightIndex = nums.length;
+
         while (leftIndex <= rightIndex) {
-            System.out.println(leftIndex);
-            System.out.println(rightIndex);
-            System.out.println();
             int midIndex = leftIndex + ((rightIndex - leftIndex) / 2);
-            System.out.println(midIndex);
-            System.out.println();
+
             if (nums[midIndex] == target) {
                 return midIndex;
-            } else if (target < nums[midIndex]) {
+            } else if (nums[midIndex] > target) {
                 rightIndex = midIndex - 1;
             } else {
                 leftIndex = midIndex + 1;
             }
         }
-        return -1;
+
+        return elementIndex;
+
     }
 
-    public static int searchMinimumNumberIndex(int[] nums) {
+    // Helper function
+    public static int findMinElementIndex(int[] nums) {
         int leftIndex = 0;
         int rightIndex = nums.length - 1;
+
         while (leftIndex < rightIndex) {
             int midIndex = leftIndex + ((rightIndex - leftIndex) / 2);
-            if (midIndex > 0 && (nums[midIndex - 1] > nums[midIndex])) {
+
+            if (midIndex > 0 && nums[midIndex - 1] > nums[midIndex]) {
                 return midIndex;
-            } else if (nums[leftIndex] <= nums[midIndex] && nums[rightIndex] < nums[midIndex]) {
+            } else if (nums[midIndex] > nums[leftIndex] && nums[midIndex] > nums[rightIndex]) {
                 leftIndex = midIndex + 1;
             } else {
                 rightIndex = midIndex - 1;
             }
         }
+
         return leftIndex;
+
     }
 
     public static void main(String[] args) {
-
         int[] nums = { 4, 5, 6, 7, 0, 1, 2 };
 
-        System.out.println(searchElementIndex(nums, 0));
+        System.out.println("Element Index is: " + searchIndex(nums, 0));
     }
 }
