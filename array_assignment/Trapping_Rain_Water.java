4e0846f55030b86c51dbf09914abb1805524cdf2
Swayamshree <mohantyswayamshree@gmail.com>
2023-01-01 23:28:36 +0530

    optimized method added

--- array_assignment/Trapping_Rain_Water.java
+++ array_assignment/Trapping_Rain_Water.java
@@ -31,9 +31,32 @@ public class Trapping_Rain_Water {
         return totalWaterTrapperd;
     }
 
+    // Optimized
+    public static int trap(int[] height) {
+        int n = height.length;
+        int res = 0, l = 0, r = n - 1;
+        int rMax = height[r], lMax = height[l];
+        while (l < r) {
+            if (lMax < rMax) {
+                l++;
+                lMax = Math.max(lMax, height[l]);
+                System.out.print("!" + (lMax - height[l]) + " ");
+                res += lMax - height[l];
+            } else {
+                r--;
+                rMax = Math.max(rMax, height[r]);
+                System.out.print("#" + (rMax - height[r]) + " ");
+
+                res += rMax - height[r];
+            }
+        }
+        System.out.println();
+        return res;
+    }
+
     public static void main(String[] args) {
         int[] heights = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
 
-        System.out.println("Total rainwater trapped: " + findTrappedRainWater(heights));
+        System.out.println("Total rainwater trapped: " + trap(heights));
     }
 }
