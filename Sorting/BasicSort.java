2db6d630ecad33d459666982e9ed2c78d7469979
Swayamshree <mohantyswayamshree@gmail.com>
2023-01-16 10:10:52 +0530

    counting sort completed

--- Sorting/BasicSort.java
+++ Sorting/BasicSort.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class BasicSort {
     public static void printArray(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
@@ -51,17 +53,51 @@ public class BasicSort {
         }
     }
 
+    /**This sorting method should be use for +ve numbers, and the value should be small.
+    *ex: 1, 4, 1, 3, 2, 4, 3, 12.
+    *
+    *Here repeatation doesn't matters
+    */
+    public static void countingSort(int[] arr) {
+        int maxElement = Integer.MIN_VALUE;
+        // Find max element
+        for (int i = 0; i < arr.length; i++) {
+            maxElement = Integer.max(maxElement, arr[i]);
+        }
+
+        // Repeat counter array
+        int repeatCounterArr[] = new int[maxElement + 1];
+
+        //Count for repeat element and assign the counter as element for repeatCounter array
+        for (int i = 0; i < arr.length; i++) {
+            repeatCounterArr[arr[i]]++;
+        }
+
+        // Sorting
+        int mainArrayIndex = 0;
+        for (int i = 0; i < repeatCounterArr.length; i++) {
+            while (repeatCounterArr[i] > 0) {
+                arr[mainArrayIndex] = i;
+                repeatCounterArr[i]--;
+                mainArrayIndex++;
+            }
+
+        }
+
+        System.out.println("MAX: " + maxElement);
+    }
+
     public static void main(String[] args) {
-        int[] array = { 5, 4, 1, 3, 2 };
+        // int[] array = { 5, 4, 1, 3, 2 };
+        int[] array = { 1, 4, 1, 3, 2, 4, 3, 12 };
 
         ///
         System.out.println("Before Sort");
         printArray(array);
 
         // Sorting
-        insertionSort(array);
+        countingSort(array);
         //
-
         ///
         System.out.println("After Sort");
         printArray(array);
