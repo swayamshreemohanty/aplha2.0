6562ff49ffbdc284af7b4e15c6cc42165b9641f1
Swayamshree Mohanty <swayamshree.appdev@colourmoon.com>
2023-05-11 06:50:46 +0530

    occurance 2nd method added

--- Recursion/IndexOccurance.java
+++ Recursion/IndexOccurance.java
@@ -1,5 +1,6 @@
 class IndexOccurance {
 
+    //1st method
     private static int findLastOccurance(int numberList[], int index, int key) {
         if (index >= numberList.length) {
             return -1;
@@ -10,6 +11,21 @@ class IndexOccurance {
             return index;
         } else {
             return result;
+        }
+
+    }
+
+    //2nd method
+    private static int findLastOccuranceReverse(int numberList[], int index, int key) {
+        if (index <= 0) {
+            return -1;
+        }
+        // Compare
+        if (numberList[index] == key) {
+            return index;
+        } else {
+            // Look forward
+            return findLastOccuranceReverse(numberList, index - 1, key);
         }
 
     }
@@ -31,6 +47,7 @@ class IndexOccurance {
     public static void main(String[] args) {
         int arr[] = { 8, 3, 6, 9, 5, 10, 9, 5, 3 };
         // System.out.println(findFirstOccurance(arr, 0, 9));
-        System.out.println(findLastOccurance(arr, 0, 3));
+        // System.out.println(findLastOccurance(arr, 0, 3));
+        System.out.println(findLastOccuranceReverse(arr, (arr.length-1), 9));
     }
 }
