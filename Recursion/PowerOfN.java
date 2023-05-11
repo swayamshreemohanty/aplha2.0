cc31fbf56eec1cc1170f1986ccf20ac4c70df752
Swayamshree Mohanty <swayamshree.appdev@colourmoon.com>
2023-05-11 09:05:22 +0530

    optimized solution added

---
+++ Recursion/PowerOfN.java
@@ -0,0 +1,35 @@
+public class PowerOfN {
+
+    static int findPower(int number, int power) {
+        if (power == 0) {
+            return 1;
+        }
+        return number * findPower(number, power - 1);
+    }
+
+    // Optimize
+    static int findPowerOptimized(int number, int power) {
+        if (power == 0) {
+            return 1;
+        }
+
+        // Find the power of a half series
+        int halfPowerValue = findPowerOptimized(number, power / 2);
+
+        //Find the square of that power value
+        int halfPowerSquare = halfPowerValue * halfPowerValue;
+
+        if (power % 2 != 0) {
+            // Odd number
+            return number * halfPowerSquare;
+        } else {
+            // Even number
+            return halfPowerSquare;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findPowerOptimized(2, 5));
+    }
+}
